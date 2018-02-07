d06726125a83e9a3804c7bd1177bba65191f26fa
Joel Day <git@dayjo.me>
2018-02-07 10:39:20 +0000

    Add dist so cdns work, update readme

--- README.md
+++ README.md
@@ -1,3 +1,85 @@
 # JS Error Handler
 
-Simple script to handle js errors and send them server-side for your perusal 
+Simple script to handle js errors and send them server-side for your perusal. It catches errors and optionally sends them to your server via ajax. It stores all errors in the browsers [sessionStorage](https://developer.mozilla.org/en-US/docs/Web/API/Window/sessionStorage) making them available to your JS app too.
+
+
+__Note:__ This library has no included dependencies. If you want the AJAX posting to work, you'll need to be including either `jQuery` or `axios`. It looks for either `$.post` or `axios.post` to send the data.
+
+------
+
+## Quick Start
+Simply include the js-error-handler.js in your web page. It's avaialable using jsDelivr;
+
+```html
+<script src="https://cdn.jsdelivr.net/gh/pallant/js-error-handler/dist/js-error-handler.min.js"></script>
+```
+
+Then intialise and configure it;
+
+```html
+<script>
+// Initiate Error Handler with no AJAX
+var MyErrorHandler = new JSErrorHandler();
+</script>
+```
+
+------
+
+## Additional Options
+
+```html
+<script>
+// Initiate Error Handler with options
+var MyErrorHandler = new JSErrorHandler({
+    ajaxURL:     '',
+    extraParams: {},
+    onSaveError: function(error){ … },
+    onSave:      function(response){ … }
+});
+</script>
+```
+
+#### ajaxURL (string)
+The url to post the error to, without this, it will not attempt to save over ajax. Remember, if you include this option you must be including either `axios` or `jQuery` for it to work.
+
+#### extraParams (object)
+An object of extra parameters to send with the ajax requests, useful for sending user identifying information i.e;
+
+```
+extraParams: { userId: 12345 }
+```
+
+#### onSaveError (function)
+Callback function that runs when the ajax request fails. Passes one parameter to the callback which is the error that was thrown so it can be handled some other way (It should still be in session storage at this point).
+
+#### onSave (function)
+Callback function that runs when the save request was successful. The only parameter passed to function is the response from the ajax request.
+
+------
+
+## Programatically access errors array
+You can access the array of errors that have occured for a user over their browser session by accessing the `.sessionStorageErrors` property on your error handler object. This might be useful within the callback functions i.e.
+
+```html
+<script>
+// Initiate Error Handler with options
+var MyErrorHandler = new JSErrorHandler({
+    ajaxURL:     '/errors',
+    onSaveError: function(error){
+
+        // Failed to save, so do something with the errors
+        console.log(this.sessionStorageErrors);
+
+    },
+});
+</script>
+```
+
+------
+
+## Building Yourself
+
+1. Clone the repo
+2. Run `npm install`
+3. Run `npm run build`
+4. JS file will now be in the dist/js-error-handler.js
--- dist/js-error-handler.js
+++ dist/js-error-handler.js
@@ -1,51 +1,49 @@
+'use strict';
+
 /**
  * JS Error Handler
  * Catches javascript errors in your app. Stores them in sessionStorage,
  * immediately reports error to an ajax url
  */
-var JSErrorHandler = function(config){
+var JSErrorHandler = function JSErrorHandler(config) {
 
     var errorHandler = {
         errors: [],
-        ajaxProvider: null,
         sessionStorageErrors: [],
         config: config,
 
         /**
          * Initialise the event listener
          */
-        init: function() {
+        init: function init() {
             var self = this;
-            window.addEventListener('error', function(e){
-                let err = JSON.parse(JSON.stringify(e, ['message', 'filename', 'lineno', 'colno', 'error']));
-                err.timestamp = Date.now();
+            window.addEventListener('error', function (e) {
+                var err = JSON.parse(JSON.stringify(e, ['message', 'filename', 'lineno', 'colno', 'error']));
                 self.errors.push(err);
                 self.sessionStorageErrors.push(err);
 
                 self.save();
-
             });
 
             // Determine how to save
-            if ( typeof jQuery != 'undefined' ) {
+            if (typeof jQuery != 'undefined') {
                 this.ajaxProvider = $.post;
-            }
-            else if ( typeof axios != "undefined" ) {
+            } else if (typeof axios != "undefined") {
                 this.ajaxProvider = axios.post;
             }
 
-            if ( typeof sessionStorage != 'undefined' ) {
-                let jsErrors = sessionStorage.getItem('jsErrors');
-                if ( jsErrors ) {
+            if (typeof sessionStorage != 'undefined') {
+                var jsErrors = sessionStorage.getItem('jsErrors');
+                if (jsErrors) {
                     this.sessionStorageErrors = JSON.parse(jsErrors);
                 }
             }
         },
-        save: function() {
-            if ( typeof config.ajaxURL != 'undefined' && this.ajaxProvider ) {
+        save: function save() {
+            if (typeof config.ajaxURL != 'undefined') {
 
-                let params = {};
-                if ( this.config.extraParams ) {
+                var params = {};
+                if (this.config.extraParams) {
                     params = this.config.extraParams;
                 }
                 params.errors = this.errors;
@@ -53,32 +51,37 @@ var JSErrorHandler = function(config){
                 this.ajaxProvider(this.config.ajaxURL, params).then(this.onSave.bind(this)).catch(this.onSaveError.bind(this));
             }
 
-            if ( typeof sessionStorage != 'undefined' ) {
+            if (typeof sessionStorage != 'undefined') {
                 sessionStorage.setItem('jsErrors', JSON.stringify(this.sessionStorageErrors));
             }
         },
 
-        onSave: function(response){
-            if (typeof this.config.onSave == 'function' ){
+        onSave: function onSave(response) {
+            if (typeof this.config.onSave == 'function') {
                 this.config.onSave.call(this, response);
             }
-            
-            this.errors = [];
+
+            if (this.config.clearOnSave) {
+                this.errors = [];
+            }
+
+            console.log(response);
         },
 
-        onSaveError: function(response){
-            if (typeof this.config.onSaveError == 'function' ){
-                this.config.onSaveError.call(this,response);
+        onSaveError: function onSaveError(response) {
+            if (typeof this.config.onSaveError == 'function') {
+                this.config.onSaveError(response);
             }
-        },
+
+            console.log(response);
+        }
 
     };
 
     errorHandler.init();
     return errorHandler;
-
 };
 
-if ( typeof module != "undefined" ) {
+if (typeof module != "undefined") {
     module.exports = JSErrorHandler;
 }
